b9efc9c1f2f1f290723cf78970d280ba56f90e56
Inalegwu Ikwue <ikwueinalegwu@gmail.com>
2023-09-17 19:54:34 +0000

    implemented messages and handling messages in nodes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io::Result;
+use std::net::{TcpListener, TcpStream};
 
+#[derive(Debug)]
 enum MessageType {
     Connect,
     AcceptConnect,
@@ -10,18 +12,29 @@ enum MessageType {
     CanRecieveData,
     CanNotRecieveData,
 }
+#[derive(Debug)]
+struct Message {
+    id: String,
+    message_type: MessageType,
+    sender_id: String,
+    receiver_id: String,
+}
+
+impl Message {
+    fn new(message_type: MessageType, sender_id: String, receiver_id: String) -> Result<Self> {
+        return Ok(Message {
+            id: "1".to_string(),
+            message_type,
+            sender_id,
+            receiver_id,
+        });
+    }
+}
 
 #[derive(Debug)]
 struct Node {
     id: String,
-    connections: Vec<&String>,
-}
-
-struct Message {
-    id: String,
-    message_type: MessageType,
-    sender_id: Option<String>,
-    receiver_id: Option<String>,
+    connections: Vec<String>,
 }
 
 impl Node {
@@ -32,24 +45,43 @@ impl Node {
         })
     }
 
-    fn handleMessage(&mut self, message: Message) {
+    fn handle_message(&mut self, message: Message) {
         match message.message_type {
             MessageType::Connect => {}
             MessageType::AcceptConnect => {
-                self.connections.push(&message.sender_id.unwrap());
+                self.connections.push(message.sender_id.clone());
                 println!(
                     "Connection established with node having id : {}",
-                    message.sender_id.unwrap()
+                    message.sender_id
                 );
             }
             MessageType::DeclineConnect => {}
-            _ => {}
+            MessageType::Data => {}
+            MessageType::WillSendData => {}
+            MessageType::WillNotSendData => {}
+            MessageType::CanRecieveData => {}
+            MessageType::CanNotRecieveData => {}
         }
     }
-    fn send_message(&self, reciever_id: String, message_type: MessageType) {}
+    fn send_message(self, receiver_id: String, message_type: MessageType) {
+        let message: Message = Message::new(message_type, self.id, receiver_id).unwrap();
+        println!("{:?}", message)
+    }
+
+    fn send_data(&self, reciever_id: &String) {
+        let mut _connection: &String;
+        for i in 0..self.connections.len() {
+            let found = self.connections.get(i).unwrap();
+            if found == reciever_id {
+                println!("found Node with id : {:?}", found);
+                _connection = found;
+            }
+        }
+    }
 }
 
 fn main() {
     let node = Node::new().unwrap();
-    println!("Hello, world! {:?}", node);
+
+    node.send_message("2".to_string(), MessageType::Connect);
 }
